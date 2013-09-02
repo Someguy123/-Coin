971e717dd3c1523106666d4536e1588cd2c2e1fc
Sykh <myself@schaunwama.de>
2013-09-02 17:12:12 +0200

    Add Accounts
    Ability to create a new account, not just addresses for the basic "" account.

--- address.php
+++ address.php
@@ -19,10 +19,10 @@ $(document).on("click", ".open-EditAddrDialog", function () {
      var myAddrId = $(this).data('id');
      $("#myAddress").html(myAddrId);
      $(".modal-body #myAddress").val( myAddrId );
-     
+
      var myAddrName = $(this).data('name');
      $(".modal-body #AddrName").val( myAddrName );
-     
+
     $('#EditAddrDialog').modal('show');
 });
 
@@ -33,6 +33,12 @@ if (isset($_POST['addaddr']) && isset($_POST['account']))
 {
   $nmc->getnewaddress($_POST['account']);
 }
+
+if (isset($_POST['addacc']) && isset($_POST['account']))
+{
+  $nmc->getaccountaddress($_POST['account']);
+}
+
 
 $myaddresses = file("myaddresses.csv");
 $myaddress_arr = array();
@@ -46,34 +52,39 @@ foreach ($myaddresses as $line)
 
 if (isset($_POST['AddrName']) && isset($_POST['myAddress']))
 {
-	$myaddress_arr[$_POST['myAddress']] = $_POST['AddrName'];
-		
-	$f = fopen("myaddresses.csv", "w");
-	
-	foreach ($myaddress_arr as $address => $name)
-	{
-	    $line = $address.";".$name."\n";		
-	    fputs($f, $line);
-	}
-	fclose($f);
+        $myaddress_arr[$_POST['myAddress']] = $_POST['AddrName'];
+
+        $f = fopen("myaddresses.csv", "w");
+
+        foreach ($myaddress_arr as $address => $name)
+        {
+            $line = $address.";".$name."\n";
+            fputs($f, $line);
+        }
+        fclose($f);
 }
 
 
 $addr = $nmc->listaccounts();
 // $addrkeys = array_keys($addr);
 echo "<div class='content'>
-<h2>Select an account to get a list of an addresses</h2>
-<form action='address.php' method='POST'>
+<h2>Select an account to get a list of an addresses</h2>";
+echo "<form action='address.php' method='POST'>
+<input name='account'>
+<input class='btn' name='addacc' type='submit' value='Add Account' />
+</form>";
+
+echo "<form action='address.php' method='POST'>
 <select name='account'>";
 foreach ($addr as $account => $balance)
 {
-	$selected = "";
-	if (isset($_POST['account']))
-	{
-	   settype($account, "string");
+        $selected = "";
+        if (isset($_POST['account']))
+        {
+           settype($account, "string");
        if ($_POST['account'] == $account)
           $selected = "selected";
-	}
+        }
     echo "<option value='{$account}' $selected>{$account} ({$balance})</option>";
 }
 echo "</select>
@@ -86,46 +97,43 @@ if (isset($_POST['account']))
 else
     $account = "";
 
-	echo "<table class='table-striped table-bordered table-condensed table'>
+        echo "<table class='table-striped table-bordered table-condensed table'>
 <thead><tr><th colspan='2'>Addresses for Account '".$account."'</th></tr></thead>";
-	foreach ($nmc->getaddressesbyaccount($account) as $address)
-	{
-		$address_label = $myaddress_arr[$address];
-		echo "<tr><td>" . $address . "</td>
-    		      <td>" . $address_label . "</td>
-		          <td><a data-id='".$address."' data-name='".$address_label."' data-toggle='modal' href='#EditAddrDialog' class='open-EditAddrDialog btn btn-mini'>Edit</a></td></tr>";
-	}
-	echo "</table>"; 
+        foreach ($nmc->getaddressesbyaccount($account) as $address)
+        {
+                $address_label = $myaddress_arr[$address];
+                echo "<tr><td>" . $address . "</td>
+                      <td>" . $address_label . "</td>
+                          <td><a data-id='".$address."' data-name='".$address_label."' data-toggle='modal' href='#EditAddrDialog' class='open-EditAddrDialog btn btn-mini'>Edit</a></td></tr>";
+        }
+        echo "</table>";
 ?>
 
 
 <form action='address.php' method='POST'>
 <!-- Modal --->
 <div id="EditAddrDialog" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		<h3 id="myModalLabel">Change Address Name</h3>
-	</div>
-	<div class="modal-body">
-	<table><tr>
-	  <td><div id="myAddress">Address to change</div></td>
-	  <td>&nbsp; &nbsp;<input type="text" name="AddrName" id="AddrName" value="Name"/></td>
-	</tr></table>
-	<input type="hidden" name="myAddress" id="myAddress" value="Nothing"/>	
-	<input type="hidden" name="account" id="account" value="<?php echo $account ?>"/>	
-	</div>
-	<div class="modal-footer">
-		<button class="btn" data-dismiss="modal">Close</button>
-		<button class="btn btn-primary">Save Changes</button>
-	</div>
+        <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h3 id="myModalLabel">Change Address Name</h3>
+        </div>
+        <div class="modal-body">
+        <table><tr>
+          <td><div id="myAddress">Address to change</div></td>
+          <td>&nbsp; &nbsp;<input type="text" name="AddrName" id="AddrName" value="Name"/></td>
+        </tr></table>
+        <input type="hidden" name="myAddress" id="myAddress" value="Nothing"/>
+        <input type="hidden" name="account" id="account" value="<?php echo $account ?>"/>
+        </div>
+        <div class="modal-footer">
+                <button class="btn" data-dismiss="modal">Close</button>
+                <button class="btn btn-primary">Save Changes</button>
+        </div>
 </div>
 </form>
-<?php    
+<?php
 echo"</div>";
 
 echo "</div>";
 include ("footer.php");
 ?>
-
-
-
